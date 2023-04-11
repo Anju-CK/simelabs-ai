fbe31d98d8498f23e8ffa3013797cbea979ce4dc
Anju <anju.ck@simelabs.com>
2023-04-11 15:18:15 +0530

    table for registered faces created

--- src/components/face-detection/fd-registered-faces/index.tsx
+++ src/components/face-detection/fd-registered-faces/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useApi from "../../../hooks/useApi";
+
 export default function Registeredfaces() {
   const { data, error, fetchData } = useApi(
     "/face_detection/registered_faces/",
@@ -7,9 +8,57 @@ export default function Registeredfaces() {
     undefined,
     false
   );
+
   useEffect(() => {
     fetchData();
-    console.log(data);
   }, []);
-  return <div></div>;
+
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setTableData(
+        data.map((item: any, index: number) => ({
+          id: index + 1,
+          name: item.name,
+          designation: item.designation,
+          email: item.email,
+        }))
+      );
+    }
+  }, [data]);
+
+  
+
+  // const handleClick=()=>{
+
+  // }
+
+  return (
+    <div>
+      <h1>Registered Faces</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Designation</th>
+            <th>Email</th>
+            {/* <th></th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((item:any) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.designation}</td>
+              <td>{item.email}</td>
+              {/* <td><button onClick={handleClick}>Delete</button></td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
