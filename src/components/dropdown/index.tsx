6d36bc2cbdbc1def6e963b465cda8cc235e3e466
Anju <anju.ck@simelabs.com>
2023-04-11 09:47:54 +0530

    indexing id corrected

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -18,7 +18,7 @@ export default function Dropdown(props: any) {
   const id = location.state?.index || 0;
   return (
     <div>
-      {id===0 && props.name.replace(/\s/g, "").toLowerCase() === "facedetection" && props.options.map((info: string, index: number) => (
+      {id===1 && props.name.replace(/\s/g, "").toLowerCase() === "facedetection" && props.options.map((info: string, index: number) => (
         <div className={styles.container} key={index}>
           <div
             className={`${styles.tab} ${open === index && styles.tabOpen}`}
@@ -37,7 +37,7 @@ export default function Dropdown(props: any) {
             )}
         </div>
       ))}
-      {id===1 && props.name.replace(/\s/g, "").toLowerCase() === "objectdetection" && props.options.map((info: string, index: number) => (
+      {id===0 && props.name.replace(/\s/g, "").toLowerCase() === "objectdetection" && props.options.map((info: string, index: number) => (
         <div className={styles.container} key={index}>
           <div
             className={`${styles.tab} ${open === index && styles.tabOpen}`}
