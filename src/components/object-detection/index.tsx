3f02e1a654bc8900d1e28ac4afc028e9425903da
Anju <anju.ck@simelabs.com>
2023-04-13 15:58:30 +0530

    useFetch hook for file upload added

--- src/components/object-detection/index.tsx
+++ src/components/object-detection/index.tsx
@@ -1,17 +1,22 @@
-import useApi from "../../hooks/useApi";
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import { INITIAL_VALUES, VALIDATION_SCHEMA } from "./schema";
-import styles from "./Objectdetection.module.css"
+import styles from "./Objectdetection.module.css";
+import useFetch from "../../hooks/useFetch";
 
 export default function Objectdetection() {
-  const { data, error, fetchData } = useApi(
+  const { data, error, fetchData } = useFetch(
     "/object_detection/identify_obj/",
     "POST",
     undefined,
     false
   );
+
   const onSubmitHandler = (values: any) => {
-    fetchData();
+    const { image } = values;
+    const formData = new FormData();
+    formData.append("image", image as File);
+    fetchData(formData);
+    console.log("data:", data);
   };
 
   return (
@@ -21,25 +26,20 @@ export default function Objectdetection() {
         validationSchema={VALIDATION_SCHEMA}
         onSubmit={onSubmitHandler}
       >
-        {({
-          handleSubmit,
-          handleBlur,
-          handleChange,
-          values,
-          errors,
-          touched,
-        }) => (
+        {({ handleSubmit, handleBlur, setFieldValue }) => (
           <Form onSubmit={handleSubmit}>
             <div className={styles.container}>
-              <Field
+              <input
                 id="image"
                 type="file"
                 name="image"
-                values={values.image}
                 placeholder="Image"
-                error={errors.image && touched.image}
                 required
-                onChange={handleChange}
+                onChange={(event: any) => {
+                  console.log("event:", event.currentTarget.files?.[0] || null);
+                  const file = event.currentTarget.files?.[0] || null;
+                  setFieldValue("image", file);
+                }}
                 onBlur={handleBlur}
                 className={styles.inputbox}
               />
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+const port = process.env.REACT_APP_PORT_NO;
+
+const endpoint = `${apiUrl}${port ? `:${port}` : ""}`;
+
+interface ApiResponse<T> {
+  data: {
+    [key: string]: any;
+  } | null;
+  error: string | null;
+  loading: boolean;
+  fetchData: (
+    values?: any,
+    onSuccess?: (res: any) => void,
+    onError?: (res: any) => void
+  ) => void;
+}
+
+type headerOptions = {
+  Authorization?: string;
+  [key: string]: any;
+};
+
+function useFetch<T>(
+  url: string,
+  method: "POST" | "PUT" | "DELETE" | "GET",
+  params?: Record<any, any>,
+  authNotRequired?: boolean
+): ApiResponse<T> {
+  const token = Cookies.get("token");
+  const [data, setData] = useState<{ [key: string]: any } | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const fetchData = async (
+    body: any,
+    onComplete?: (res: any) => void,
+    onError?: (msg: string) => void
+  ) => {
+    setLoading(true);
+
+    const headers: headerOptions = {};
+
+    if (!authNotRequired) {
+      headers["Authorization"] = `Token ${token}`;
+    }
+    const queryParams = params
+      ? `?${new URLSearchParams(params).toString()}`
+      : "";
+    const fullUrl = `${endpoint}${url}${queryParams}`;
+
+    try {
+      console.log("body:", body);
+      console.log("url:", fullUrl);
+      const response = await fetch(fullUrl, {
+        method,
+        headers: {
+          ...headers,
+        },
+        body: body,
+      });
+      const responseData = await response.json();
+      if (!response?.status.toString().startsWith("2")) {
+        setError(responseData);
+        if (responseData?.status === 401) {
+          Cookies.remove("token");
+          const queryParams = { q: "unauthorized" };
+          const queryString = new URLSearchParams(queryParams).toString();
+          navigate(`/?${queryString}`);
+        }
+        if (typeof onError === "function") {
+          onError(responseData);
+        }
+      } else {
+        setData(responseData);
+        if (typeof onComplete === "function") {
+          onComplete(responseData);
+        }
+        setError(null);
+      }
+    } catch (err) {
+      setData(null);
+      setError("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, error, loading, fetchData };
+}
+
+export default useFetch;
