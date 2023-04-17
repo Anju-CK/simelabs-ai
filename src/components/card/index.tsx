cb6049358f51d87aad105bd422cb05c670b1c3fe
Anju <anju.ck@simelabs.com>
2023-04-17 17:04:45 +0530

    demo component added

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,5 @@
 import { capitalize } from "../../utils/capitalize";
+import { Demo } from "../demo";
 import { Subscriptionform } from "../subscription-form";
 import styles from "./Card.module.css";
 
@@ -6,30 +7,48 @@ export default function Card(props: any) {
   const handleClick = () => {
     props.onClick();
   };
-  if (props.hit_limit <= 0) {
-    return null;
-  }
+
+  // if (props.hit_limit <= 0) {
+  // return null;
+  // }
   return (
-    <div className={styles.container} >
-      <div className={styles.container1} onClick={handleClick}>{capitalize(props.name)}</div>
-      <div className={styles.box}>
+    <div className={styles.container}>
+      {props.subscribed == 0 && props.hit_limit == 0 ? (
         <div>
-          <div className={styles.container2}>
-            <div className={styles.text}>Hit Limit</div>
-            <div className={styles.text}>Current Usage</div>
-            <div className={styles.text}>Expiry</div>
+          <div className={styles.container3}>{capitalize(props.name)}</div>
+
+          <div className={styles.btn}>
+            <Demo model_url={props.model_url} />
+            <Subscriptionform model_url={props.model_url} />
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className={styles.container1} onClick={handleClick}>
+            {capitalize(props.name)}
           </div>
 
-          <div className={styles.container2}>
-            <div className={styles.text}>{props.hit_limit}</div>
-            <div className={styles.text}>{props.current_usage}</div>
-            <div className={styles.text}>{props.expiry}</div>
+          <div className={styles.box}>
+            <div>
+              <div className={styles.container2}>
+                <div className={styles.text}>Hit Limit</div>
+                <div className={styles.text}>Current Usage</div>
+                <div className={styles.text}>Start Date</div>
+                <div className={styles.text}>Expiry</div>
+              </div>
+
+              <div className={styles.container2}>
+                <div className={styles.text}>{props.hit_limit}</div>
+                <div className={styles.text}>{props.current_usage}</div>
+                <div className={styles.text}>{props.start_date}</div>
+                <div className={styles.text}>{props.expiry}</div>
+              </div>
+            </div>
+
+            <div className={styles.cancel}>Cancel</div>
           </div>
-        </div>
-        <div className={styles.subscribe}>
-          <Subscriptionform model_url={props.model_url} />
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/demo/index.tsx
@@ -0,0 +1,109 @@
+import { useOutsideClick } from "../../hooks/useOutsideClick";
+import { useRef, useState, useContext } from "react";
+import formStyles from "./Demo.module.css";
+import { usePopper } from "react-popper";
+import useApi from "../../hooks/useApi";
+import { DetailContext } from "../../context/detailContext";
+type Placement =
+  | "auto"
+  | "auto-start"
+  | "auto-end"
+  | "top"
+  | "top-start"
+  | "top-end"
+  | "bottom"
+  | "bottom-start"
+  | "bottom-end"
+  | "right"
+  | "right-start"
+  | "right-end"
+  | "left"
+  | "left-start"
+  | "left-end";
+
+export const Demo = ({
+  size = "large",
+  popupPlacement = "bottom-start",
+  onSuccess,
+  model_url,
+}: {
+  size?: "small" | "large";
+  popupPlacement?: Placement;
+  onSuccess?: (data: any) => void;
+  model_url?: string;
+}) => {
+  const [referenceElement, setReferenceElement] =
+    useState<HTMLButtonElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
+    null
+  );
+  const context: any = useContext(DetailContext);
+  const [open, setOpen] = useState<boolean>(false);
+  const popperWrapperRef = useRef(null);
+  const popperBtnDivRef = useRef(null);
+
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: popupPlacement,
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [0, 18],
+        },
+      },
+    ],
+  });
+
+  const togglePopper = (e?: any) => {
+    e && e.preventDefault();
+    e && e.stopPropagation();
+    setOpen(!open);
+  };
+
+  useOutsideClick(popperWrapperRef, setOpen, popperBtnDivRef);
+  const { data, error, fetchData } = useApi(
+    "/users/demo/",
+    "POST",
+    undefined,
+    false
+  );
+  const onSubmitHandler = () => {
+    setOpen(!open);
+    fetchData({ model_url: model_url }, (res: any) => {
+      typeof context.test === "function" && context.test();
+    });
+  };
+  return (
+    <>
+      <div ref={popperBtnDivRef}>
+        <button
+          type="button"
+          ref={setReferenceElement}
+          className={formStyles.demobtn}
+          onClick={togglePopper}
+        >
+          Demo
+        </button>
+      </div>
+      {open && (
+        <div
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          <div ref={popperWrapperRef}>
+            <div className={formStyles.container}>
+              Would you like to raise the hit limit and try demo?
+              <div className={formStyles.btn}>
+                <button className={formStyles.yesbtn} onClick={onSubmitHandler}>
+                  Yes
+                </button>
+                <button className={formStyles.nobtn}>No</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
