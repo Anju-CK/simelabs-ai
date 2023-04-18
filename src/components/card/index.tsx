9a660b07617488b638d4c650c30b9dad254049e9
Anju <anju.ck@simelabs.com>
2023-04-18 11:30:09 +0530

    modifications in card component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -11,22 +11,13 @@ export default function Card(props: any) {
   };
   return (
     <div className={styles.container}>
-      {props.subscribed == 0 && props.hit_limit == 0 ? (
-        <div>
-          <div className={styles.container3}>{capitalize(props.name)}</div>
-
-          <div className={styles.btn}>
-            <Demo model_url={props.model_url} />
-            <Subscriptionform model_url={props.model_url} />
-          </div>
-        </div>
-      ) : (
+      {props.subscribed == 1 ? (
         <>
           <div className={styles.box}>
             <div className={styles.container1} onClick={handleClick}>
               {capitalize(props.name)}
             </div>
-            <img src={subscribe} style={{ width: "30px", height: "30px" }} />
+            <img src={subscribe} style={{ width: "30px", height: "30px" }} title="Subscribed" />
           </div>
           <div className={styles.box}>
             <div>
@@ -50,6 +41,41 @@ export default function Card(props: any) {
             </div>
           </div>
         </>
+      ) : props.subscribed == 0 && props.hit_limit != 0 ? (
+        <>
+          <div className={styles.box}>
+            <div className={styles.container1} onClick={handleClick}>
+              {capitalize(props.name)}
+            </div>
+          </div>
+          <div className={styles.box}>
+            <div>
+              <div className={styles.container2}>
+                <div className={styles.text}>Hit Limit</div>
+                <div className={styles.text}>Current Usage</div>
+                <div className={styles.text}>Start Date</div>
+                <div className={styles.text}>Expiry</div>
+              </div>
+
+              <div className={styles.container2}>
+                <div className={styles.text}>{props.hit_limit}</div>
+                <div className={styles.text}>{props.current_usage}</div>
+                <div className={styles.text}>{props.start_date}</div>
+                <div className={styles.text}>{props.expiry}</div>
+              </div>
+            </div>
+            <Subscriptionform model_url={props.model_url} />
+          </div>
+        </>
+      ) : (
+        <div>
+          <div className={styles.container3}>{capitalize(props.name)}</div>
+
+          <div className={styles.btn}>
+            <Demo model_url={props.model_url} />
+            <Subscriptionform model_url={props.model_url} />
+          </div>
+        </div>
       )}
     </div>
   );
