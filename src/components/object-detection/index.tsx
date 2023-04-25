3449dd42d0678278073cb3c8dc223473793c5e5e
Anju <anju.ck@simelabs.com>
2023-04-25 13:33:48 +0530

    popup added to obj-detection

--- src/components/object-detection/index.tsx
+++ src/components/object-detection/index.tsx
@@ -2,8 +2,11 @@ import { ErrorMessage, Form, Formik } from "formik";
 import { INITIAL_VALUES, VALIDATION_SCHEMA } from "./schema";
 import styles from "./Objectdetection.module.css";
 import useFetch from "../../hooks/useFetch";
+import { useState } from "react";
+import Modalcomponent from "../modalcomponent";
 
 export default function Objectdetection() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { data, error, fetchData } = useFetch(
     "/object_detection/identify_obj/",
     "POST",
@@ -17,7 +20,7 @@ export default function Objectdetection() {
     formData.append("image", image as File);
     fetchData(formData);
   };
-  console.log("data:", data);
+  console.log("Test:", data);
 
   return (
     <div className={styles.container}>
@@ -29,6 +32,7 @@ export default function Objectdetection() {
         {({ handleSubmit, handleBlur, setFieldValue }) => (
           <Form onSubmit={handleSubmit}>
             <div className={styles.container}>
+              <label className={styles.image}>Select Image</label>
               <input
                 id="image"
                 type="file"
@@ -54,6 +58,9 @@ export default function Objectdetection() {
               <button
                 type="submit"
                 className={styles.btntxt + " " + styles.detect}
+                onClick={() => {
+                  setIsModalOpen(true);
+                }}
               >
                 Detect
               </button>
@@ -61,10 +68,12 @@ export default function Objectdetection() {
                 type="reset"
                 className={styles.btntxt + " " + styles.cancel}
                 onClick={() => {
-                  const input = document.getElementById("image") as HTMLInputElement;
+                  const input = document.getElementById(
+                    "image"
+                  ) as HTMLInputElement;
                   if (input) {
                     input.value = "";
-                    setFieldValue("image", null); 
+                    setFieldValue("image", null);
                   }
                 }}
               >
@@ -74,6 +83,64 @@ export default function Objectdetection() {
           </Form>
         )}
       </Formik>
+      <Modalcomponent
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+       
+        {data ? (
+          <div className={styles.modalbox}>
+            <div>
+              <div className={styles.heading}>MDConfidence </div>{" "}
+              {data.MDConfidence ? `${data.MDConfidence}` : 0}
+            </div>
+
+            <div>
+              <div className={styles.heading}>MaskCord </div>
+              {data && data.MaskCord
+                ? data.MaskCord.map((cord: any) => <div key={cord}>{cord}</div>)
+                : 0}
+            </div>
+
+            <div>
+              <div className={styles.heading}>GogglesConfidence </div>{" "}
+              {data.GogglesConfidence ? `${data.GogglesConfidence}` : 0}
+            </div>
+            <div>
+              <div className={styles.heading}>GogglesCord </div>
+              {data && data.gogglesCord
+                ? data.gogglesCord.map((cord: any) => (
+                    <div key={cord}>{cord}</div>
+                  ))
+                : 0}
+            </div>
+
+            <div>
+              <div className={styles.heading}>PPEConfidence </div>{" "}
+              {data.PPEConfidence ? `${data.PPEConfidence}` : 0}
+            </div>
+            <div>
+              <div className={styles.heading}>PPECord </div>
+              {data && data.ppeCord
+                ? data.ppeCord.map((cord: any) => <div key={cord}>{cord}</div>)
+                : 0}
+            </div>
+
+            <div>
+              <div className={styles.heading}>HelmetConfidence</div>{" "}
+              {data.HelmetConfidence ? `${data.HelmetConfidence}` : 0}
+            </div>
+            <div>
+              <div className={styles.heading}>HelmetCord </div>
+              {data && data.helmetCord
+                ? data.helmetCord.map((cord: any) => (
+                    <div key={cord}>{cord}</div>
+                  ))
+                : 0}
+            </div>
+          </div>
+        ) : null}
+      </Modalcomponent>
     </div>
   );
 }
