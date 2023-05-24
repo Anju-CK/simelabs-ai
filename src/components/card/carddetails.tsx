717164497368dbc6e8fef8201688bc4be71100e8
Anju <anju.ck@simelabs.com>
2023-05-24 16:15:34 +0530

    home page made responsive

--- src/components/card/carddetails.tsx
+++ src/components/card/carddetails.tsx
@@ -2,7 +2,7 @@ import styles from "./Card.module.css";
 
 export default function CardDetails({ data }: any) {
   return (
-    <div>
+    <div className={styles.layout}>
       <div className={styles.container2}>
         <div className={styles.text}>Hit Limit</div>
         <div className={styles.text}>Current Usage</div>
@@ -10,7 +10,7 @@ export default function CardDetails({ data }: any) {
         <div className={styles.text}>Expiry</div>
       </div>
 
-      <div className={styles.container2}>
+      <div className={styles.container5}>
         <div className={styles.text}>{data.hit_limit}</div>
         <div className={styles.text}>{data.current_usage}</div>
         <div className={styles.text}>{data.start_date}</div>
